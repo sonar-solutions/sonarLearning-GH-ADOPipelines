beb17928cfbe2cb1418c47d5431fd630624ec9cd
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2025-03-31 14:48:55 -0500

    Update s1940.py

--- src/s1940.py
+++ src/s1940.py
@@ -21,3 +21,19 @@ def fun(a):
   i = 10
   return i + a       # Noncompliant
   i += 1             # this is never executed
+
+
+def fun(b):
+  i = 10
+  return i + b       # Noncompliant
+  i += 1             # this is never executed
+
+def fun(c):
+  i = 10
+  return i + c       # Noncompliant
+  i += 1             # this is never executed
+
+def fun(d):
+  i = 10
+  return i + d      # Noncompliant
+  i += 1             # this is never executed
